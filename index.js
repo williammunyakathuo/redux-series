4fd92c51f14f0bcc5f3c1bb7c0add74f09b9a23a
williammunyakathuo <tjwellieyam@gmail.com>
2023-08-21 22:34:01 +0300

    redux practice

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const redux = require('redux');
 const createStore = redux.createStore
+const bindActionCreators = redux.bindActionCreators
 
 
 // REDUX ACTION
 const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+const ICE_ORDERD = 'ICE_ORDERD'
+const ICE_RESTOCKED = 'ICE_RESTOCKED'
 
 
 function orderCake() {
@@ -13,8 +17,30 @@ function orderCake() {
     }
 }
 
+function restockCake(qty = 1) {
+    return {
+        type: CAKE_RESTOCKED,
+        quantity: qty
+    }
+}
+
+function orderIcream(qty = 1) {
+    return {
+        type: ICE_ORDERD,
+        payload: qty
+    }
+}
+
+function restockIce(qty = 1) {
+    return {
+        type: ICE_RESTOCKED,
+        payload: qty
+    }
+}
+
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numberOfIce: 20
 }
 
 
@@ -24,7 +50,23 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case CAKE_ORDERED:
             return {
+                ...state,
                 numOfCakes: state.numOfCakes - 1,
+            }
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.quantity,
+            }
+        case ICE_ORDERD:
+            return {
+                ...state,
+                numberOfIce: state.numberOfIce - 1,
+            }
+        case ICE_RESTOCKED:
+            return {
+                ...state,
+                numberOfIce: state.numberOfIce + action.payload,
             }
         default:
             return state
@@ -32,15 +74,29 @@ const reducer = (state = initialState, action) => {
 }
 
 
+
+
 const store = createStore(reducer)
 
-console.log(' initial state' , store.getState())
+console.log(' initial state', store.getState())
 
-const unsubscribe =  store.subscribe(()=> console.log(' updated state', store.getState()))
+const unsubscribe = store.subscribe(() => console.log(' updated state', store.getState()))
 
-store.dispatch( orderCake())
-store.dispatch( orderCake())
-store.dispatch( orderCake())
-store.dispatch( orderCake())
+// store.dispatch( orderCake())
+// store.dispatch( orderCake())
+// store.dispatch( orderCake())
+// store.dispatch( orderCake())
+// store.dispatch(restockCake(3))
+
+const actions = bindActionCreators({ orderCake, restockCake, orderIcream, restockIce }, 
+    store.dispatch)
+
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(3)
+actions.orderIcream()
+actions.orderIcream()
+actions.restockIce(2)
 
 unsubscribe()
